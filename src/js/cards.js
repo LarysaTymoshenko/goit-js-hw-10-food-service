df9ef91c6b0e2adadf6cbae8831a80b5c759035e
LarysaTymoshenko <luda76@ukr.net>
2021-09-25 23:10:25 +0300

    Create cart list

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,14 +1,12 @@
 
-
-
 import cards from './menu.json'
-// import cardsTpl from './templates/cards.hbs'
-// console.log(cards);
+import cardsTpl from '../templates/cards.hbs'
+console.log(cards);
 
 
 cards.map(el=>console.log(el))
-// const createCardListMarkup = (cards) => {
-//     return cardsTpl(cards);
-// }
+const createCardListMarkup = (cards) => {
+    return cardsTpl(cards);
+}
 
-// export const cardListMarkup = createCardListMarkup(cards);
+export const cardListMarkup = createCardListMarkup(cards);
